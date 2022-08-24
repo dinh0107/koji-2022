b37d2095f9cfdc8cb708564f716beba0ced911d9
dinh0107 <dinhdv01072001@gmail.com>
2022-08-24 16:21:36 +0700

    update

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ $(document).ready(function () {
         speed: 1500,
         slidesToShow:1,
         slidesToScroll: 1,
-        // autoplay: true,
+        autoplay: true,
         autoplaySpeed: 2000,
         variableWidth: true,
         prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-left"></i></button>',
